a91974aaefcbab6958a767dd9a2715414b0eccea
김규년 <42989067+ggn724@users.noreply.github.com>
2022-05-02 14:10:04 +0900

    src update

--- src/study/P2_2_Code.java
+++ src/study/P2_2_Code.java
@@ -3,24 +3,21 @@ package study;
 import java.util.Arrays;
 
 public class P2_2_Code {
-    public boolean solution(int[] arr){
-        // 배열 정렬
-        Arrays.sort(arr,0,arr.length);
-        //1. 배열에 중복 이 있는지 확인
-        for(int i =0; i < arr.length; i++){
-            for(int j=0; j<i; j++){
-                if(arr[i] == arr[j]) return false;
-            }
-            //2. 배열 내 길이만큼 값이 차례대로 들어갔는지 확인
-            if(arr[i] != i+1) return false;
-        }
-        return true;
-    }
+	public boolean solution(int[] arr) {
+		Arrays.sort(arr, 0, arr.length);
+		// 1. 배열에 중복 이 있는지 확인
+		// 2. 배열 내 길이만큼 값이 차례대로 들어갔는지 확인
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != i + 1)
+				return false;
+		}
+		return true;
+	}
 
-    public static void main(String[] args){
-        int[] arr = new int[]{4,1,3};
-        P2_2_Code p2 = new P2_2_Code();
-        boolean bVal = p2.solution(arr);
-        System.out.println(bVal);
-    }
+	public static void main(String[] args) {
+		int[] arr = new int[] { 4, 1, 3 ,2};
+		P2_2_Code p2 = new P2_2_Code();
+		boolean bVal = p2.solution(arr);
+		System.out.println(bVal);
+	}
 }
