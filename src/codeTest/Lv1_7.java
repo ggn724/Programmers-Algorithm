94c058ebbfc6e6286c54fbc740d5817f9f2d63c0
김규년 <42989067+ggn724@users.noreply.github.com>
2022-05-11 23:34:36 +0900

    Update Lv1_7.java

--- src/codeTest/Lv1_7.java
+++ src/codeTest/Lv1_7.java
@@ -1,52 +1,80 @@
 package codeTest;
 
+import java.lang.reflect.Array;
 import java.util.*;
+import java.util.stream.Stream;
 
 public class Lv1_7 {
-
     public int[] solution(int[] answers) {
-        // 수포자들 정답 횟수 count...
-        List<Integer> listPerson = new ArrayList<>();
-        Map<String, Integer> getCountP1 = new HashMap<>();
-        Map<String, Integer> getCountP2 = new HashMap<>();
-        Map<String, Integer> getCountP3 = new HashMap<>();
+        // 정답 맞힌 횟수
+        int getCntP1 = 0;
+        int getCntP2 = 0;
+        int getCntP3 = 0;
 
-        // 수포자1 set
-        int i = 1;
-        for(int j=0; j<answers.length; j++){
-                if(i > 5) i = 1;
-                if(i == answers[j]){
-                    getCountP1.put("p1", getCountP1.getOrDefault("p1",0)+1);
-                }
-                i++;
-            }
+        // 10번만 돌면 누가 1등인지 판단됨.
 
-        // 수포자2 set
-        int i2 = 1;
-        for(int j=0; j<answers.length; j++){
-            if(j%2 == 0){
-                if(answers[j] == i) {
-                    getCountP2.put("p2", getCountP2.getOrDefault("p2", 0)+1);
-                }
-                i++;
-                if(i == 2) i++;
-                if(i > 5) i=1;
-            } else if(answers[j] == 2){
-                getCountP2.put("p2", getCountP2.getOrDefault("p2", 0)+1);
-            }
+        // 수포자 값 SET
+        int[] P1 = new int[]{1,2,3,4,5,1,2,3,4,5};
+        int[] P2 = new int[]{2,1,2,3,2,4,2,5,2,1};
+        int[] P3 = new int[]{3,3,1,1,2,2,4,4,5,5};
+
+        for(int i=0; i < answers.length; i++) {
+            if(answers[i] == P1[i]) getCntP1++;
+            if(answers[i] == P2[i]) getCntP2++;
+            if(answers[i] == P3[i]) getCntP3++;
         }
-        listPerson.add(getCountP1.get("p1"));
-        listPerson.add(getCountP2.get("p2"));
 
-        int[] answer = listPerson.stream().mapToInt(Integer::intValue).toArray();
+        int result = tmpfunc(getCntP1,getCntP2,getCntP3);
+
+        int[] answer = Stream.of(String.valueOf(result).split("")).mapToInt(Integer::parseInt).toArray();
         return answer;
+    }
+
+    int tmpfunc(int a, int b, int c){
+        String iRet = "";
+        int result = 0;
+
+        if(a == b) {
+            if(a == c) iRet = "abc";
+            else iRet = (a > c) ? "ab":"c";
+        } else if(a > b) {
+            if(a == c) iRet = "ac";
+            else iRet = (a > c) ? "a":"c";
+        } else {
+            if(b == c) iRet = "bc";
+            else iRet = (b > c) ? "b":"c";
+        }
+
+        switch(iRet){
+            case "abc":
+                result = 123;
+                break;
+            case "ab":
+                result = 12;
+                break;
+            case "ac":
+                result = 13;
+                break;
+            case "bc":
+                result = 23;
+                break;
+            case "a":
+                result = 1;
+                break;
+            case "b":
+                result = 2;
+                break;
+            case "c":
+                result = 3;
+                break;
+        }
+        return result;
     }
 
 
 
-
     public static void main(String[] args) {
-        int[] tmp = new int[]{1,2,3,4,5};
+        int[] tmp = new int[]{1,3,2,4,2};
         Lv1_7 s7 = new Lv1_7();
         int[] iRet =  s7.solution(tmp);
         System.out.println(Arrays.toString(iRet));
