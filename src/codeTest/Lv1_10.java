9668a145b7fb7bf2ef86d20cb3b43be5c170c896
김규년 <42989067+ggn724@users.noreply.github.com>
2022-05-13 17:09:00 +0900

    Create Lv1_10.java

---
+++ src/codeTest/Lv1_10.java
@@ -0,0 +1,31 @@
+package codeTest;
+
+import java.util.Arrays;
+
+public class Lv1_10 {
+
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+
+        // 배열 Sort
+        Arrays.sort(d);
+        System.out.println(Arrays.toString(d));
+
+        // while 돌리면서 값빼주고 cnt하기
+        for (int i = 0; 0 < budget; i++) {
+            if (budget >= d[i]) {
+                budget -= d[i];
+                answer++;
+            } else break;
+        }
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        int[] a = new int[]{1,3,2,5,4};
+        Lv1_10 sp = new Lv1_10();
+        int iRet = sp.solution(a, 9);
+        System.out.println(iRet);
+    }
+}
