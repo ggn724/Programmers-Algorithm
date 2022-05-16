d725d1de7e67f2a63484939a405e30e597d03849
ggn724 <724ggn@gmail.com>
2022-05-16 09:24:20 +0900

    Create Lv1_20.java

---
+++ src/CodingTest/Level1/두개뽑아서더하기/Lv1_20.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * */
+
+package CodingTest.Level1.두개뽑아서더하기;
+
+public class Lv1_20 {
+    public int[] solution(int[] numbers) {
+        int[] answer = {};
+        return answer;
+    }
+
+
+    // Test
+    public static void main(String[] args){
+
+    }
+}
